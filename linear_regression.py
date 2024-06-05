ba991a03a60e17073a7ec3fe5ff3a8861c38a6ce
done-n-dusted <31320.anurag@gmail.com>
2024-06-05 02:03:26 -0400

    updated boiler for linear regression as well

--- linear_regression.py
+++ linear_regression.py
@@ -6,14 +6,15 @@ class LinearRegression:
         self.n_iters = n_iters
 
     def fit(self, X, y):
-        self.weights = np.zeros(X.shape[1])
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
         self.bias = 0
 
         for _ in range(self.n_iters):
             ypred = np.dot(X, self.weights) + self.bias
 
-            dw = (2 / len(X))*np.dot(X, (ypred - y))
-            db = (2 / len(X))*np.sum(ypred - y)
+            dw = (2 / n_samples)*np.dot(X.T, (ypred - y))
+            db = (2 / n_samples)*np.sum(ypred - y)
 
             self.weights = self.weights - self.lr * dw
             self.bias = self.bias - self.lr * db
--- model_boiler.py
+++ model_boiler.py
@@ -5,19 +5,34 @@ import matplotlib.pyplot as plt
 import argparse
 
 from knearest import KNN
-
-iris = datasets.load_iris()
-
-X, y = iris.data, iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-plt.figure()
-plt.scatter(X[:, 2], X[:, 3], c=y, cmap=plt.cm.Set1, edgecolor="k")
-plt.show()
+from linear_regression import LinearRegression
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, help="Name of the model")
+parser.add_argument("--type", choices=['regression', 'classification'], help="Type of model", required=True)
 args = parser.parse_args()
+
+if args.type == 'classification':
+    iris = datasets.load_iris()
+
+    X, y = iris.data, iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    plt.figure()
+    plt.title("Iris Dataset, classification example")
+    plt.scatter(X[:, 2], X[:, 3], c=y, cmap=plt.cm.Set1, edgecolor="k")
+    plt.show()
+
+if args.type == 'regression':
+    diabetes = datasets.load_diabetes()
+
+    X, y = diabetes.data, diabetes.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    plt.figure()
+    plt.title("Diabetes Dataset, regression example")
+    plt.scatter(X[:, 2], y, color="black")
+    plt.show()
 
 if args.model == "knn":
     clf = KNN(k=3)
@@ -26,3 +41,10 @@ if args.model == "knn":
     print(predictions)
     acc = np.sum(predictions == y_test) / len(y_test)
     print(acc)
+
+if args.model == "linear-regression":
+    reg = LinearRegression(n_iters=100000, learning_rate=0.001)
+    reg.fit(X_train, y_train)
+    predictions = reg.predict(X_test)
+
+    print(np.sqrt(np.mean((predictions - y_test) ** 2)))
